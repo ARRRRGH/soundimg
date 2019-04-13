fe4bd313b40a2a3e28f6f96b2f3ffef62ffe511e
Unknown <rrrr@posteo.ch>
2019-04-14 00:31:57 +0200

    define FilterMode

--- data_structures/brush_modes.py
+++ data_structures/brush_modes.py
@@ -206,3 +206,58 @@ class AddMode(BrushMode):
 
     def show_settings_dialog(self):
         self.settings_dialog.run()
+
+
+class FilterMode(BrushMode):
+    """ Apply a PIL filter to a selected patch. """
+
+    def __init__(self):
+        BrushMode.__init__(self)
+
+        self.filter_type = 'gaussian_blur'
+        self.size = 0
+        self.axis= 0
+        self.upper_thr = 255
+        self.lower_thr = 0
+
+        settings = [('Filter Type', 'filter_type', str, 'combo',
+                     {'store': py_utils.ImageHandler.get_support_keys()}, 1, 1),
+                    ('Size', 'size', int, 'entry', {}, 1, 1),
+                    ('Axis', 'axis', int, 'entry', {'range': (0, 1)}, 1, 1),
+                    ('Upper Threshold', 'upper_thr', int, 'entry', {'range': (0, 255), 'increments': (1, 1)}, 1, 1),
+                    ('Lower Threshold', 'lower_thr', int, 'entry', {'range': (0, 255), 'increments': (1, 1)}, 1, 1)]
+
+        self.settings_dialog = base_widgets.SettingsDialog(settings, self, 'Set Threshold', config.main_window)
+
+    def apply(self, img, brush_img, mask, weights):
+        """
+        Return weighted difference of two arrays.
+
+        :param mask:
+        :param img: np.array
+        :param brush_img: np.array
+        :param weights: np.array, opacity of brush_img
+        :return:
+        """
+
+        shape = (weights.shape[0], weights.shape[1], img.shape[2])
+        bg = np.reshape(img[mask], shape).astype('float64')
+
+        br = py_utils.ImageHandler.Filter(self.filter_type, bg.astype('uint8'),
+                                                 self.size, self.axis).reshape(shape).astype('float64')
+
+        s = np.sum(bg, axis=2) / 3
+        thr_mask = np.logical_and(s <= self.upper_thr, s >= self.lower_thr)
+        not_thr_mask = np.logical_not(thr_mask)
+
+        bg = np.einsum('ijk, ij -> ijk', bg, (1 - weights))
+        br = np.einsum('ijk, ij -> ijk', br, weights)
+
+        bg[thr_mask] += br[thr_mask]
+        bg[not_thr_mask] = bg[not_thr_mask]
+
+        img[mask] = np.ravel(np.clip(bg, a_min=0, a_max=255).astype('uint8'))
+        return img
+
+    def show_settings_dialog(self):
+        self.settings_dialog.run()
